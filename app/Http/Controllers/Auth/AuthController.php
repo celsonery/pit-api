80afa22d026e5757fdbf056fe5cd8caffc1ac4db
Celso Nery <celso.nery@gmail.com>
2024-07-05 22:16:50 -0300

    Add socialite

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -5,12 +5,15 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
+use App\Models\Provider;
 use App\Models\User;
+use GuzzleHttp\Exception\ClientException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Laravel\Socialite\Facades\Socialite;
 
 class AuthController extends Controller
 {
@@ -51,14 +54,85 @@ class AuthController extends Controller
         return response()->json(['message' => 'Invalid credentials'], 403);
     }
 
-    public function logout()
+    public function logout(): JsonResponse
     {
+        Auth::user()->tokens()->delete();
 
-
+        return response()->json(['message' => 'Logged out'], 201);
     }
 
     public function me()
     {
         return auth()->user();
+    }
+
+    /**
+     * Redirect the user to the Provider authentication page.
+     *
+     * @param string $provider
+     * @return JsonResponse
+     */
+    public function redirectToProvider(string $provider)
+    {
+        $validated = $this->validateProvider($provider);
+
+        if (!$validated) {
+            return response()->json(['error' => 'Please login using facebook, github or google'], 422);
+        }
+
+        return Socialite::driver($provider)->stateless()->redirect();
+    }
+
+    /**
+     * Redirect the user to the Provider authentication page.
+     *
+     * @param string $provider
+     * @return JsonResponse
+     */
+    public function handleProviderCallback(string $provider): JsonResponse
+    {
+        $validated = $this->validateProvider($provider);
+
+        if (!$validated) {
+            return response()->json(['error' => 'Please login using facebook, github or google'], 422);
+        }
+
+        try {
+            $user = Socialite::driver($provider)->stateless()->user();
+        } catch (ClientException $exception) {
+            return response()->json(['error' => 'Invalid credentials provided'], 422);
+        }
+
+        $userCreated = User::firstOrCreate([
+            'email' => $user->getEmail()
+        ], [
+            'email_verified_at' => now(),
+            'name' => $user->getName(),
+            'status' => true,
+        ]);
+
+        $userCreated->providers()->updateOrCreate([
+            'provider' => $provider,
+            'provider_id' => $user->getId(),
+        ], [
+            'avatar' => $user->getAvatar()
+        ]);
+
+        $token = $userCreated->createToken('token-name')->plainTextToken;
+
+        return response()->json($userCreated, 200, ['access_token' => $token]);
+    }
+
+    /**
+     * @param string $provider
+     * @return boolean
+     */
+    protected function validateProvider(string $provider): bool
+    {
+        if (in_array($provider, ['facebook', 'github', 'google'])) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,42 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
 
 
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->middleware('auth:sanctum')
+    ->name('logout');
 
 Route::get('/me', [AuthController::class, 'me'])
     ->middleware('auth:sanctum')
     ->name('me');
+
+//Route::get('/oauth/redirect', function () {
+//    return Socialite::driver('github')->redirect();
+//});
+//
+//Route::get('/oauth/callback', function () {
+//    $githubUser = Socialite::driver('github')->stateless()->user();
+//
+//    $user = User::updateOfCreate([
+//       'github_id' => $githubUser->id,
+//    ],[
+//        'name' => $githubUser->name,
+//        'email' => $githubUser->email,
+//        'github_token' => $githubUser->token,
+//        'github_refresh_token' => $githubUser->refreshToken,
+//    ]);
+//
+//    return Auth::login($user);
+//
+////    return redirect('/dashboard');
+//});
+
+Route::get('/oauth/{provider}', [AuthController::class,'redirectToProvider']);
+Route::get('/oauth/{provider}/callback', [AuthController::class,'handleProviderCallback']);
