16cdf4a1d316cde23bf2223a8ec9f37dba197945
Celso Nery <celso.nery@gmail.com>
2024-07-31 19:06:27 -0300

    refactor: Change the routes names

--- routes/api.php
+++ routes/api.php
@@ -5,16 +5,16 @@ use App\Http\Controllers\Auth\PasswordResetController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'auth'], function () {
-    Route::post('/register', [AuthController::class, 'register'])->name('register');
-    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
+    Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
     Route::get('/activate/{token}', [AuthController::class, 'activate'])->name('register.activate');
     Route::post('/password/create', [PasswordResetController::class, 'create'])->name('password.reset.create');
     Route::get('/password/find/{token}', [PasswordResetController::class, 'find'])->name('password.reset.find');
     Route::post('/password/reset', [PasswordResetController::class, 'reset'])->name('password.reset');
 
     Route::group(['middleware' => 'auth:sanctum'], function () {
-        Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-        Route::get('/user', [AuthController::class, 'user'])->name('user');
+        Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+        Route::get('/user', [AuthController::class, 'user'])->name('auth.user');
 
     });
 });
