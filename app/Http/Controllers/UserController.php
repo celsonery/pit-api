b465ac26d2cff56266bf0d5b3cd4a922a083afc3
Celso Nery <celso.nery@gmail.com>
2024-11-23 22:51:47 -0300

    feat: Add update for user data

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+//use App\Http\Requests\UpdateUserRequest;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function update(Request $request): JsonResponse
+    {
+        $user = User::find($request->user()->id);
+        $user->update($request['userData']);
+
+        return response()->json(["message" => "User updated successfully!"], 200);
+    }
+}
