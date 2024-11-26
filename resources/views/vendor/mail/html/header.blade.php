7305a3c84468209897642f5cd877fd3c8de037c1
Celso Nery <celso.nery@gmail.com>
2024-11-26 09:50:32 -0300

    fix: Change the icon for email in header.blade.php

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -4,7 +4,7 @@
 <a href="{{ $url }}" style="display: inline-block;">
 {{--@if (trim($slot) === 'Laravel')--}}
 {{--<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">--}}
-<img src="https://api-pit.oregon.net.br/storage/coffee.png" class="logo" alt="Laravel Logo">
+<img src="https://api-pit.oregon.net.br/storage/images/coffee.png" class="logo" alt="Café gourmet logo">
 {{--@else--}}
 {{--{{ $slot }}--}}
 {{--@endif--}}
